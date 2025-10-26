17bcb06ff0e04b3404985333e96b6517e626f2e0
s-gabo <gabrielove11@gmail.com>
2025-10-25 23:29:41 -0300

    novos programas

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é de ${vel} km/h `)
+if (vel > 60){
+    console.log('Você está acima da velocidade permitida, VOCÊ FOI MULTADO')
+}
+console.log('Dirija sempre com o cinto de segurança afivelado!')
