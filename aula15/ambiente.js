c4a38a018e1743ed59851d36769a981cd3a61145
s-gabo <gabrielove11@gmail.com>
2025-12-06 21:50:18 -0300

    for

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+var num = [4, 8, 12, 16, 20]
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`nosso vetor é ${num}`)
+
+let pos = num.indexOf(8)
+console.log(`o valor 8 está na posição ${pos}`)
